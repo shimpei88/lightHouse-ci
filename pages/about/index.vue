1f92e15454b5ad345a99456008b2eb9b8f27dcb1
shimpei88 <bigisland793@gmail.com>
2020-02-12 10:50:03 +0900

    add: about page

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1>about</h1>
+  </div>
+</template>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
